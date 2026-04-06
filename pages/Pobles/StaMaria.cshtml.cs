3ccf295e3eddeca96c8ee096e0cf6d2ccc9d8872
totmallorca <totmallorca123ac@gmail.com>
2026-04-06 17:13:45 +0200

    Add files via upload

---
+++ pages/Pobles/StaMaria.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mallorca.Pages.Pobles
+{
+    public class StaMariaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
