b5c253a4d7829055d5f9753aceee5b56ae1120fe
totmallorca <totmallorca123ac@gmail.com>
2026-04-06 17:15:25 +0200

    Add files via upload

---
+++ pages/rutes/rutes_alaro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mallorca.Pages.rutes
+{
+    public class rutes_alaroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
