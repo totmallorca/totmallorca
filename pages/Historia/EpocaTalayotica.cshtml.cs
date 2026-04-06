a65dc74e6ddd0f46016e588538d02e184fdaecc9
totmallorca <totmallorca123ac@gmail.com>
2026-04-06 17:10:38 +0200

    Add files via upload

---
+++ pages/Historia/EpocaTalayotica.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mallorca.Pages.Historia
+{
+    public class EpocaTalayoticaModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
